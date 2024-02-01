43441c98bba7054bf683d489266e6fd360b93738
Brenda <brendakungu04@gmail.com>
2024-02-01 13:23:39 +0300

    inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,108 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
+
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+
+        self.knowledge = knowledge
+
 
     def teach(self):
-        pass
+        if not self.knowledge:
+            return "I have no knowledge to share"
+        
+        random_index = random.randit(0, len(self.knowledge) - 1)
+        return self.knowledge(random_index)
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from user import User
+
+# import random
+
+# class Teacher(User):
+
+#     def teach(self)
+    
